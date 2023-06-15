7d2983abf4007b9a386a567cca0d380049dedf61
ammainamugo <ammainamugo@gmail.com>
2023-06-15 13:50:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /*
- *puts substitute for printf
+ *project:puts substitute for printf
+ *
+ *main(void) is the entry point
+ *
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+/*and terminates when it returns zero*/
 	return (0);
 }
 
