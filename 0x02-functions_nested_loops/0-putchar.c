9cbeeb7fd34501ac7c78cc2df9949d5e13c47573
ammainamugo <ammainamugo@gmail.com>
2023-06-21 11:51:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
-#include <main.h>
-
+#include "main.h"
 /**
  * main - Program entry point print -putchar
  * return = Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +14,5 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return 0;
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifdef main_h
-#define main_h
+#ifdef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet-x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
