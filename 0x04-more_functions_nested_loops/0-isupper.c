21bc7384b4983b14d944a10eb50c022759fd972b
ammainamugo <ammainamugo@gmail.com>
2023-06-22 13:49:22 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,12 @@
 /**
  * _isupper - check for uppercase character
  * @c: value to check between A and Z
- * Return: 1 if upper, 0 otherwise
+ * Return: 1 if upper, 0 if lower
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);/* c is uppercase */
-	} else
-	{
+	else
 		return (0);/* c is lowercase */
-	}
-
 }
