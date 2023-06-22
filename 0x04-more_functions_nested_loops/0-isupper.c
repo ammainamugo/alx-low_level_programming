a0d33eb70d09d61645002504f669d537dae18ac2
ammainamugo <ammainamugo@gmail.com>
2023-06-22 06:19:47 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c: value to check between A and Z
+ * Return: 1 if upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);/* c is uppercase */
+	} else
+	{
+		return (0);/* c is lowercase */
+	}
+
+}
