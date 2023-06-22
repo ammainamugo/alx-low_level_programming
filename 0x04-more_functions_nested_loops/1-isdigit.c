c8ad2110123577de05fe3b84c6d2b0c0e489f4ac
ammainamugo <ammainamugo@gmail.com>
2023-06-22 06:37:25 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,11 +7,9 @@
  */
 int _isdigit(int c)
 {
-	if (c >=0 && c <=9)
-	{
-		return (1); /* c is a digt */
-	} else
-	{
-		return (0); /* c is not a digit */
-	}
+	if (c >= '0' && c <= '9')
+		return (1); // c is a digit
+	else
+		return (0); //c is not a digit
 }
+
