08b6cb7f5074bd31c9252c3e4b9c11e540e602f0
ammainamugo <ammainamugo@gmail.com>
2023-06-23 15:21:14 -0700

    1-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,9 +7,20 @@
 
 int main(void)
 {
-int i;
+	int i=0;
 
-i = 0;
-positive_or_negative(i);
-return (0);
+        int positive_or_negative(i);
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        }
+        else if (i == 0)
+        {
+                printf("%d is zero\n", i);
+        }
+        else
+        {
+                printf("%d is negative\n", i);
+        }
+        return (0);
 }
