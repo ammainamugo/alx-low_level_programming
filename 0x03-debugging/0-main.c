902fda7c95ef1cba3b41a96e6f464fa5e605fa86
ammainamugo <ammainamugo@gmail.com>
2023-06-23 16:17:41 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,12 @@
 #include "main.h"
-
 /**
-* main - program entry point
-* Return: 0 success
+* main - Test function for positive or negative
+* Return: 0
 */
-
 int main(void)
 {
-	int i=0;
-
-        int positive_or_negative(i);
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else
-        {
-                printf("%d is negative\n", i);
-        }
-        return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
