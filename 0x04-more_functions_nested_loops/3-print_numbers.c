5f571e54ff6a28f58997561d9ab613284b534117
ammainamugo <ammainamugo@gmail.com>
2023-06-23 05:37:28 -0700

    3 push so many times

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_number() - print a number zero to nine
- * @a: value of number to be printed
+ * void print_numbers - print values 0 to 9
+ * return: void
  */
 
 void print_numbers(void)
 {
 	char c = '0';
 
-	while(c <= '9')
+	while (c <= '9')
 	{
 		_putchar(c);
 		c++;
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 10 times
+ * return: void
+ */
+
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0;
+}
