6fd546f064705b8b0b95bc40d0f3bf837dbd5ffc
ammainamugo <ammainamugo@gmail.com>
2023-06-27 13:40:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: value stored in the pointer
+ * Return: Always 0 success
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
+
