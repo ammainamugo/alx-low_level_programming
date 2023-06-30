d0b6729055a969dcc9b2e22e848cab45a1e80af5
ammainamugo <ammainamugo@gmail.com>
2023-06-30 13:59:41 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,20 +4,22 @@
  * _strncpy - function that copies a string
  * @dest: value to append
  * @src: input value
+ * @n: byte size
  * Return: new value
  */
 
-char *_strncpy(char *dest, char *src, int n) {
-    int i;
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
-
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
-
-    return dest;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
