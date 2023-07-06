4b87cd23763b5dc51231afa8d9c03b9cd4c3cd5d
ammainamugo <ammainamugo@gmail.com>
2023-07-06 01:15:33 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	char c = *s;
-
-	if (c == '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
-		_putchar(c);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 	_putchar('\n');
 }
